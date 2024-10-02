bf6c790db600a96e4dbb3ed90f0764d92211e0cb
Michael De Guzman <michaelydeguzman@gmail.com>
2024-10-02 17:37:51 +1000

    Fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@
 import StarryBackground from "./components/background/starryBg";
 import { NavBar } from "./components/navBar/navBar";
 import { Sections } from "./components/sections/sections";
+import useIsMobile from "./hooks/useIsMobile";
 
 export default function Home() {
+  const isMobile = useIsMobile();
   return (
     <div className="relative flex h-full w-full flex-col items-center">
       <NavBar />
-      <StarryBackground />
+
+      {isMobile ? null : <StarryBackground />}
+
       <Sections />
     </div>
   );
